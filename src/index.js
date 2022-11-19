0720b4d03f21fed146c5e7dcf6377754268e1de6
Luiz Rosane <lphilippirosane@gmail.com>
2022-11-19 19:38:59 +0100

    #1 Generate schedule from the dentists and booking info

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-const db = require("./communication/db.js");
-const mqttClient = require("./communication/mqtt.js")
+const { db, data } = require("./communication/db.js");
+const mqttClient = require("./communication/mqtt.js");
+const filter = require("./tools/filter.js");
+
+const t = '{"from": "2022-11-18", "to": "2022-11-21" }'
+setTimeout(
+  () =>
+    console.log(
+      filter.generateSchedule(data.dentists, data.bookings, t)[0].schedule
+    ),
+  1000
+);
+
---
+++ src/tools/filter.js
@@ -0,0 +1,131 @@
+const REG = /(\d{1,2})(?=\:)/g;
+const REG_SORTING = /(\d{1,2})(?=\:)/;
+const DAY = 1000 * 60 * 60 * 24; //day in milliseconds
+const BREAKES = 2;
+const WEEK_DAYS = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wenesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+function generateSchedule(dentists, bookings, interval) {
+    interval = parseDate(interval)
+  var intervalLength = (interval.to - interval.from) / DAY;
+  console.log(intervalLength);
+  var generalSchedule = [];
+
+  for (let j = 0; j < intervalLength; j++) {
+    var schedule = new Map();
+    var date = new Date(interval.from.getTime() + j * DAY);
+
+    // filter bookings according to their date
+    var filteredBookings = filterBookings(bookings, date);
+
+    dentists.forEach((dentist) => {
+      pushDentistAvailableSlots(schedule, dentist, filteredBookings, date);
+    });
+
+    const stringDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    const unsortedSchedule = Object.fromEntries(schedule);
+
+    const sortedSchedule = sortTimeSlots(unsortedSchedule);
+
+    generalSchedule.push({
+      date: stringDate,
+      schedule: sortedSchedule,
+    });
+  }
+
+  return generalSchedule;
+}
+
+function parseDate(stringInterval) {
+  return JSON.parse(stringInterval, (key, value) => {
+    if (key) {
+      return new Date(value);
+    } else {
+      return value;
+    }
+  });
+}
+
+function filterBookings(bookings, date) {
+  var filteredBookings = [];
+  bookings.map((booking) => {
+    if (booking.date.toString() === date.toString()) {
+      filteredBookings.push(booking);
+    }
+  });
+  return filteredBookings;
+}
+
+// idea for sorting the object keys source: https://bobbyhadz.com/blog/javascript-sort-object-keys
+function sortTimeSlots(unsortedSchedule) {
+  const sortedSchedule = Object.keys(unsortedSchedule)
+    .sort((first, second) => {
+      const a = first.match(REG_SORTING)[0];
+      const b = second.match(REG_SORTING)[0];
+      return a - b;
+    })
+    .reduce((sorted, key) => {
+      sorted[key] = unsortedSchedule[key];
+      return sorted;
+    }, {});
+
+  return sortedSchedule;
+}
+
+function pushDentistAvailableSlots(schedule, dentist, bookings, date) {
+  var openinghours = dentist.openinghours[WEEK_DAYS[date.getDay()]];
+  if (openinghours) {
+    var [open, close] = openinghours.match(REG);
+    open = parseInt(open, 10);
+    close = parseInt(close, 10);
+
+    for (var i = 0; i < close - open; i++) {
+      var time = `${open + i}:00-${open + i}:30`;
+      var timeHalf = `${open + i}:30-${open + i + 1}:00`;
+
+      var alreadyTaken = 0;
+      var alreadyTakenHalf = 0;
+
+      bookings.map((booking) => {
+        if (booking.dentistsid == dentist.id && booking.time == time) {
+          alreadyTaken += 1;
+        }
+
+        if (booking.dentistsid == dentist.id && booking.time == timeHalf) {
+          alreadyTakenHalf += 1;
+        }
+      });
+
+      var entry = {
+        dentist: dentist.id,
+        slots: dentist.dentists - alreadyTaken,
+      };
+
+      var entryHalf = {
+        dentist: dentist.id,
+        slots: dentist.dentists - alreadyTakenHalf,
+      };
+
+      if (schedule.has(time)) {
+        schedule.get(time).push(entry);
+      } else {
+        schedule.set(time, [entry]);
+      }
+
+      if (schedule.has(timeHalf)) {
+        schedule.get(timeHalf).push(entryHalf);
+      } else {
+        schedule.set(timeHalf, [entryHalf]);
+      }
+    }
+  }
+}
+
+module.exports = { generateSchedule };
