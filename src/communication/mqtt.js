b32cc3dab5d4789bf9339c68c06e4414a038d304
Luiz Rosane <lphilippirosane@gmail.com>
2022-11-22 16:18:56 +0100

    Revert "#1 Generate current week interval for the initial schedule"
    This reverts commit 5f3e42d60ebaa33fc5837d5a53c56262b1d02823.
    
     #1 Revert generation of schedule interval by the schedule handler
    
     After discussing with the team members responsible for the token
     handler component, we decided that the token handler will pass the
     current week interval and the GUI will publish it in the initial
     schedule request.

--- src/communication/mqtt.js
+++ src/communication/mqtt.js
@@ -3,8 +3,6 @@ const { db, data } = require("./db.js");
 const Booking = require("../models/booking.js");
 const filter = require("../tools/filter.js");
 
-const DAY = 1000 * 60 * 60 * 24; //day in milliseconds
-
 const HOST = "mqtt://localhost";
 const PORT = 1883;
 
@@ -28,14 +26,13 @@ mqttClient.on("message", (topic, message) => {
 
   switch (topic) {
     case sub_topics.initialSchedule:
-      const interval = getCurrentWeekInterval();
+      const interval = parseDate(message);
       publishInitialSchedule(interval);
-      break;
+    // fetch bookings from this interval
   }
 });
 
 async function publishInitialSchedule(interval) {
-  //get all the bookings from the desired interval
   const bookings = await Booking.find({
     date: { $gte: interval.from, $lte: interval.to },
   });
@@ -56,35 +53,6 @@ function parseDate(stringInterval) {
       return value;
     }
   });
-}
-
-function getCurrentWeekInterval() {
-  var today = new Date();
-
-  today.setHours(1, 0, 0, 0);
-
-  //returns a number from 0 (sunday) to saturday according to the mdn wev docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
-  //however we want that each week starts in a monday and ends in a sunday
-  const dayOfTheWeek = today.getDay();
-
-  var start, end;
-  if (dayOfTheWeek == 0) {
-    start = new Date(today.getTime() - 7 * DAY);
-    end = today;
-    end.setHours(23, 59, 59);
-  } else {
-    start = new Date(today.getTime() - DAY * (dayOfTheWeek - 1));
-    end = new Date(today.getTime() + DAY * (7 - dayOfTheWeek));
-    end.setHours(23, 59, 59);
-  }
-  //set the end hours to 23:59:59 to include all of its bookings
-
-  var interval = {
-    from: start,
-    to: end,
-  };
-
-  return interval;
 }
 
 module.exports = mqttClient;
